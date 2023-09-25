0174d5dd246b428ea27c7a7a621df20ff7c5eddc
Reyhan <74343144+reyhanehvr@users.noreply.github.com>
2023-09-25 03:52:47 -0700

    Add files via upload
    Head First Java (Third Edition)
    This repository is about the Head First Java book exercise solution

---
+++ Head-First-Java/src/chapter_01/CodeMagnet.java
@@ -0,0 +1,22 @@
+package chapter_01;
+
+public class CodeMagnet {
+    // Head First Java Code Magnet Exercise Solution
+    public static void main(String[] args){
+        int x = 3;
+        while(x > 0){
+            if (x > 2){
+                System.out.print("a");
+            }
+            x = x -1;
+            System.out.print("-");
+            if (x == 2){
+                System.out.print("b c");
+            }
+            if (x == 1 ){
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
---
+++ Head-First-Java/src/chapter_10/StaticSuper.java
@@ -0,0 +1,38 @@
+package chapter_10;
+
+public class StaticSuper {
+    static {
+        System.out.println("super static block");
+    }
+
+    StaticSuper(){
+        System.out.println("super constructor");
+    }
+}
+
+class StaticTests extends StaticSuper{
+    static int rand;
+
+    static {
+        rand = (int) (Math.random() * 6);
+        System.out.println("static block "+ rand);
+    }
+
+    StaticTests(){
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("in main");
+        StaticTests st = new StaticTests();
+
+        /*
+        The Output will be
+        super static block
+        static block (random number)
+        in main
+        super constructor
+        constructor
+         */
+    }
+}
